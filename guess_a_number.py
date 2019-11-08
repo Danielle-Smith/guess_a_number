b7524b9839829479e1037c7624ee5011e1d5ec49
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2019-11-08 15:08:51 -0700

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -5,7 +5,7 @@ random_choice = random.randint(0,10)
 user_choice = input("Can you guess the number I'm thinking of? 1-10: ")
 
 
-while int(user_choice) != (random_choice):
+while int(user_choice) != int(random_choice):
         print('Try again')
         user_choice = input("Can you guess the number I'm thinking of? 1-10: ")
 
